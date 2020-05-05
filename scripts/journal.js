6ec5de6e5be2f78849f251b11401831a286609ad
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-05-04 21:53:32 -0500

    daily-journal-2

---
+++ scripts/journal.js
@@ -0,0 +1,13 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const dailyJournal = []
+const JournalEntry = {
+    date: "5/4/20",
+    concepts: "Es6 modules",
+    entry: "keep track of the study",
+    mood: "okay"
+}
+dailyJournal.push(journalEntry)
+
